bd6bc3bd04ae1717f467a2706f9da456bf4245f0
Eakram Shah <eakramalishah@gmail.com>
2025-05-13 09:41:41 +0530

    python files

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Hello,coders !! ")
+Name = input ("what is your name ? \n")
+print ("hi\n" + Name + "\nyou are great !! ")
---
+++ joke.py
@@ -0,0 +1,28 @@
+import random
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 150)  # Set speaking speed
+
+jokes = [
+    "Why don’t scientists trust atoms? Because they make up everything!",
+    "Why did the computer go to art school? Because it had a lot of bytes!",
+    "What do you call a bear with no teeth? A gummy bear!",
+    "Why was the math book sad? Because it had too many problems.",
+    "Why did the robot go on vacation? Because it needed to recharge!"
+]
+
+print ("Welcome to the Joke World wanna hear a Joke\n")
+answer = input().strip().lower()
+
+if answer == "yes":
+  name = input ("\nWhat is you name\n")
+  print ("Hi\n" + name + " Here is a joke for you.\n")
+  joke = random.choice(jokes)
+  print(joke)
+  engine.say(joke)
+  engine.runAndWait()
+else:
+  print ("\nOk no problem ! Maybe Next time ")
+  engine.say("\n Ok no problem ! Maybe Next time ")
+  engine.runAndWait()
