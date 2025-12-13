2f9cc60cae5cabdf4c34ad1ebe8e1f0a76e63bb5
Eakram Shah <eakramalishah@gmail.com>
2025-12-13 14:10:49 +0530

    added files

---
+++ golang/calculator.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func calculate(a int, b int) []float64 {
+  // your code goes here
+	fmt.Println(a+b)
+	return results
+}
+
+func main() {
+	fmt.Println(calculate(20, 10))
+	fmt.Println(calculate(700, 70))
+}
